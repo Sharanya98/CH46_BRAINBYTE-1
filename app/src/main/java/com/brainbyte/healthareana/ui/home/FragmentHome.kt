edd9a3ff823155b48cb34bc8941093b695b28c8f
Aditya Mahakalkar <adityamahakalkar49@gmail.com>
2020-07-26 14:16:52 +0530

    Add account achievements

--- app/src/main/java/com/brainbyte/healthareana/ui/home/FragmentHome.kt
+++ app/src/main/java/com/brainbyte/healthareana/ui/home/FragmentHome.kt
@@ -1,12 +1,23 @@
 package com.brainbyte.healthareana.ui.home
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.ImageSpan
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.brainbyte.healthareana.R
 import com.brainbyte.healthareana.databinding.FragmentBaseBinding
+import com.brainbyte.healthareana.databinding.ItemScoreBinding
+import com.brainbyte.healthareana.util.Truss
 
 class FragmentHome : Fragment() {
 
@@ -18,17 +29,63 @@ class FragmentHome : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentBaseBinding.inflate(layoutInflater, container, false)
-
         return binding.root
     }
 
+    // TODO:: insert data idhar
+    private val listOfScore = listOf(
+        ScoreModel(
+            R.drawable.ic_baseline_arrow_forward_24,
+            "BMI",
+            240,
+            123,
+            listOf(R.drawable.ic_coin, R.drawable.ic_coin, R.drawable.ic_coin, R.drawable.ic_coin)
+        ),
+        ScoreModel(
+            R.drawable.ic_baseline_arrow_forward_24,
+            "BMI",
+            240,
+            123,
+            listOf(R.drawable.ic_coin, R.drawable.ic_coin, R.drawable.ic_coin, R.drawable.ic_coin)
+        ),
+        ScoreModel(
+            R.drawable.ic_baseline_arrow_forward_24,
+            "BMI",
+            240,
+            123,
+            listOf(R.drawable.ic_coin, R.drawable.ic_coin, R.drawable.ic_coin, R.drawable.ic_coin)
+        ),
+        ScoreModel(
+            R.drawable.ic_baseline_arrow_forward_24,
+            "BMI",
+            240,
+            123,
+            listOf(R.drawable.ic_coin, R.drawable.ic_coin, R.drawable.ic_coin, R.drawable.ic_coin)
+        ),
+        ScoreModel(
+            R.drawable.ic_baseline_arrow_forward_24,
+            "BMI",
+            240,
+            123,
+            listOf(R.drawable.ic_coin, R.drawable.ic_coin, R.drawable.ic_coin, R.drawable.ic_coin)
+        ),
+        ScoreModel(
+            R.drawable.ic_baseline_arrow_forward_24,
+            "BMI",
+            240,
+            123,
+            listOf(R.drawable.ic_coin, R.drawable.ic_coin, R.drawable.ic_coin, R.drawable.ic_coin)
+        )
+    )
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-       binding.profileProgressBar.setProgressWithAnimation(85f, 5000)
-
         binding.apply {
+            profileProgressBar.setProgressWithAnimation(85f, 5000)
+            fragmentAccountContainer.recyclerView.apply {
+                adapter = ScoreAdapter(listOfScore)
+                layoutManager = LinearLayoutManager(requireContext())
+            }
             fragmentHomeContainer.apply {
                 bmiGameButton.setOnClickListener {
                     findNavController().navigate(FragmentHomeDirections.actionFragmentHomeToFragmentBMI())
@@ -47,5 +104,60 @@ class FragmentHome : Fragment() {
             }
         }
 
+    }
+}
+
+data class ScoreModel(
+    @DrawableRes val icon: Int,
+    val title: String,
+    val data: Int,
+    val coins: Int,
+    val badges: List<Int>
+)
+
+class ScoreAdapter(private val list: List<ScoreModel>) : RecyclerView.Adapter<ScoreViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScoreViewHolder =
+        ScoreViewHolder(
+            ItemScoreBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: ScoreViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+}
+
+class ScoreViewHolder(private val binding: ItemScoreBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(model: ScoreModel) = binding.apply {
+        icon.setImageDrawable(ContextCompat.getDrawable(icon.context, model.icon))
+        titleText.text = Truss().pushSpan(AbsoluteSizeSpan(18, true))
+            .pushSpan(StyleSpan(Typeface.BOLD))
+            .append(model.title)
+            .popSpan()
+            .popSpan()
+            .pushSpan(AbsoluteSizeSpan(16, true))
+            .append("\n${model.data}")
+            .build()
+        coinsText.text = model.coins.toString()
+
+
+        val truss = Truss().append("Badge\n")
+        model.badges.forEach {
+            val drawable = ContextCompat.getDrawable(badgeTextView.context, it)?.apply {
+                setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+            }
+            truss
+                .pushSpan(ImageSpan(drawable!!, ImageSpan.ALIGN_BASELINE))
+                .append(" ")
+                .popSpan()
+                .append(" ")
+        }
+        badgeTextView.text = truss.build()
     }
 }
